669210b7a7f39f4e66b5aa7146480b84c4606e27
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-09-17 13:34:30 +0200

    Lección 4.4 - @Scope

--- demo/src/main/java/com/openwebinars/demo/MainDeMentira.java
+++ demo/src/main/java/com/openwebinars/demo/MainDeMentira.java
@@ -2,6 +2,9 @@ package com.openwebinars.demo;
 
 import com.openwebinars.demo.autowired.MiServicio;
 import com.openwebinars.demo.beans.Query;
+import com.openwebinars.demo.scope.Servicio1;
+import com.openwebinars.demo.scope.Servicio2;
+import com.openwebinars.demo.scope.Servicio3;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,20 +20,34 @@ public class MainDeMentira {
     //@Autowired
     //private MiServicio miServicio;
 
+    //@Autowired
+    //@Qualifier("apiQuery")
+    //private Query query;
+
     @Autowired
-    @Qualifier("apiQuery")
-    private Query query;
+    private Servicio1 s1;
+
+    @Autowired
+    private Servicio2 s2;
+
+    @Autowired
+    private Servicio3 s3;
 
     @PostConstruct
     public void init() {
 
-        /*for (String s : ctx.getBeanDefinitionNames()) {
+        for (String s : ctx.getBeanDefinitionNames()) {
             System.out.println(">>> "+ s);
-        }*/
+        }
+
+        System.out.println(s1.utils);
+        System.out.println(s2.utils);
+        System.out.println(s3.utils);
+
 
         //miServicio.ejecutar();
 
-        System.out.println(query.fetchData());
+       // System.out.println(query.fetchData());
 
     }
 
---
+++ demo/src/main/java/com/openwebinars/demo/scope/Servicio1.java
@@ -0,0 +1,13 @@
+package com.openwebinars.demo.scope;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Servicio1 {
+
+    @Autowired
+    public Utils utils;
+
+
+}
