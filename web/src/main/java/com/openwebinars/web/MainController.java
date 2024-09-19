83c3f25a8db7ebfe38edca13ef7084ec120fc232
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-09-19 12:49:19 +0200

    Lección 5.4 (2) - Controlador con datos

--- web/src/main/java/com/openwebinars/web/MainController.java
+++ web/src/main/java/com/openwebinars/web/MainController.java
@@ -1,13 +1,15 @@
 package com.openwebinars.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("message", "Hola Mundo!");
         return "index";
     }
 
