9caf5bb4159fa36bf73769f33246941143c38879
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-09-26 19:34:51 +0200

    Lección 6.3 - Repositorios

---
+++ data/src/main/java/com/openwebinars/data/MainDeMentira.java
@@ -0,0 +1,24 @@
+package com.openwebinars.data;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MainDeMentira {
+
+    @Autowired
+    private ProductoRepository repo;
+
+
+    @PostConstruct
+    public void run() {
+
+        Producto p = new Producto(null, "Un producto", 123.45);
+
+        repo.save(p);
+
+
+    }
+
+}
---
+++ data/src/main/java/com/openwebinars/data/ProductoRepository.java
@@ -0,0 +1,6 @@
+package com.openwebinars.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
