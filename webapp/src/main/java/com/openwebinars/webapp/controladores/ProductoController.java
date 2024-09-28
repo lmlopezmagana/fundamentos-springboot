92a5532c8c5afaf068fce3f68c5cf6c0970a2bff
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-09-28 23:27:26 +0200

    Lección 7.3 - Listado por consultas

--- webapp/src/main/java/com/openwebinars/webapp/controladores/ProductoController.java
+++ webapp/src/main/java/com/openwebinars/webapp/controladores/ProductoController.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class ProductoController {
@@ -16,6 +20,17 @@ public class ProductoController {
     public String todosLosProductos(Model model) {
         model.addAttribute("productos", repository.findAll());
         return "index";
+    }
+
+    @GetMapping("/categoria/{cat}")
+    public String productosPorCategoria(@PathVariable("cat") String categoria, Model model) {
+        model.addAttribute("productos", repository.findByCategoriaIgnoreCase(categoria));
+        return "index";
+    }
+
+    @ModelAttribute("categorias")
+    public List<String> categorias() {
+        return repository.listarCategorias();
     }
 
 }
--- webapp/src/main/java/com/openwebinars/webapp/repositorios/ProductoRepository.java
+++ webapp/src/main/java/com/openwebinars/webapp/repositorios/ProductoRepository.java
@@ -2,6 +2,16 @@ package com.openwebinars.webapp.repositorios;
 
 import com.openwebinars.webapp.entidades.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+
+    @Query("select distinct p.categoria from Producto p")
+    List<String> listarCategorias();
+
+    List<Producto> findByCategoriaIgnoreCase(String categoria);
+
 }
